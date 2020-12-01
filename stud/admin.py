368cca62665766ffb1d3f9a0aa1719698121fb9a
Christiansly <50357321+Christiansly@users.noreply.github.com>
2020-12-01 21:00:40 +0100

    Update admin.py

--- stud/admin.py
+++ stud/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Course
-# Register your models here.
+# Register your models here
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
   list_display = ('course_code', 'semester', 'grade', 'unit')
